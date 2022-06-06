7e1c616ff6fbb6020d4b3ee79b335e94c5cd860d
denilsonB <denilson1569@hotmail.com>
2022-06-06 15:56:23 -0300

    create visit factorybot

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,17 @@
 FactoryBot.define do
     factory :user do
-      
       sequence(:name) { |n| "user#{n}" }
-      sequence(:email) { |n| "user#{n}@example.com" }
+      sequence(:email) { Faker::Internet.email}
       sequence(:cpf) { Faker::CPF.numeric }
       password { "Password3" }
       password_confirmation {'Password3'}
+    end
+    
+    factory :visit do 
+      sequence(:date) {Faker::Date.between(from: Date.today,to: 2.days.from_now)}
+      sequence(:status) {rand(0..2)}
+      association :user
+      sequence(:checkin_at) {Faker::Time.between(from: 3.days.ago,to: 2.days.ago)}
+      checkout_at {1.day.ago}
     end
   end
