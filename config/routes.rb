054ae505adac0aa6c7af109af771ba7c608a88fd
denilsonB <denilson1569@hotmail.com>
2022-06-07 16:15:00 -0300

    add visits resources to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
+  resources :users do
+    resources :visits
+  end
   post '/auth/login', to: 'authentication#login'
 end
