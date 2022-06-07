964409a962445c01c089d3ec8aa9abf5b0bd3746
denilsonB <denilson1569@hotmail.com>
2022-06-07 16:14:34 -0300

    create visit controller

---
+++ app/controllers/visits_controller.rb
@@ -0,0 +1,68 @@
+class VisitsController < ApplicationController
+    unless Rails.env.test?
+        before_action :authorize_request
+    end
+    before_action :current_user
+    before_action :verify_creator, only: [:update, :destroy]
+
+    #GET /visits
+    def index
+        @visits = @user.visits
+        render json: @visits, status: :ok
+    end
+
+    #GET /visits/{id}
+    def show
+        @visit = @user.visits.find(params[:id])
+        render json: @visit, status: :ok
+    end
+
+    #POST /visits
+    def create
+        @service = VisitServices::Create.call(visit_params)
+        render_service
+    end
+    
+    #PUT /visits/{id}
+    def update
+        @service = VisitServices::Update.call(params[:id],visit_params)
+        render_service
+    end
+
+    #DELETE /visits/{id}
+    def destroy
+        @visit = Visit.find(params[:id])
+        @visit.destroy
+        render json: {"message":"visit deleted with success!"}
+    end
+    
+    private
+    def verify_creator
+        @visit = Visit.find(params[:id])
+        unless @visit.user == @current_user
+            render json: {"message":"User not allowed"}
+        end
+    end       
+    
+    def current_user
+        @user = User.find(params[:user_id])
+        unless @user == @current_user
+            render json: {"message":"User not allowed"}
+        end
+    end
+
+    def render_service
+        if @service.success?
+          render json: @service.result, status: :ok
+        else
+          render json: {  **@service.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def visit_params
+        params.permit(
+            :date, :status, :user_id, :checkin_at, :checkout_at
+        )
+    end
+
+end
