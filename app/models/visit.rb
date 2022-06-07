2bf2b059593f0a3b99155fbbb388f81caeeaed80
denilsonB <denilson1569@hotmail.com>
2022-06-06 23:46:21 -0300

    add validations to visit model

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,5 +3,9 @@ class Visit < ApplicationRecord
   acts_as_paranoid
 
   enum status: {PENDENTE: 0, REALIZANDO: 1, REALIZADA: 2}
-
+  validates :user, presence: true
+  validates :date, date: { after_or_equal_to: Proc.new { Date.current}}
+  validates :checkin_at, date: {before: Proc.new {DateTime.current}}
+  validates :checkin_at, date: {before: Proc.new {|obj| obj.checkout_at}}
+  validates :checkout_at, date: {after: Proc.new {|obj| obj.checkin_at}}
 end
