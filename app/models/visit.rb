ff924e1f2fc804d1e4a07884e4d49d2e2cf10777
denilsonB <denilson1569@hotmail.com>
2022-06-11 13:22:23 -0300

    update checkin_at validation

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -5,7 +5,7 @@ class Visit < ApplicationRecord
   enum status: {PENDENTE: 0, REALIZANDO: 1, REALIZADA: 2}
   validates :user, presence: true
   validates :date, date: { after_or_equal_to: Proc.new { Date.current}}
-  validates :checkin_at, date: {before: Proc.new {DateTime.current}}
+  validates :checkin_at, date: {before_or_equal_to: Proc.new {Date.current}}
   validates :checkin_at, date: {before: Proc.new {|obj| obj.checkout_at}}
   validates :checkout_at, date: {after: Proc.new {|obj| obj.checkin_at}}
 
