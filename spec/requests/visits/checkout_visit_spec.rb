93e49b41d8475f38c4c706898294b806ac2800cf
denilsonB <denilson1569@hotmail.com>
2022-06-11 13:22:44 -0300

    create checkout spec

---
+++ spec/requests/visits/checkout_visit_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Visits', type: :request do
+    let!(:my_user){FactoryBot.create(:user)}
+    let!(:my_visit){FactoryBot.create(:visit,user: my_user)}
+    let!(:token) {JsonWebToken.encode(user_id: my_user.id)}
+
+    context 'Checkout Visit ' do
+        describe 'POST Visit checkout', type: :request do
+            before do
+                post "/users/#{my_user.id}/visits/#{my_visit.id}/checkout", params: {}, headers: {
+                "Authorization" => "#{token}"
+            }
+            end
+            
+            it "is valid with status code 200" do
+                p json
+                expect(response.status).to eq(200)
+            end
+        end
+    end
+end
