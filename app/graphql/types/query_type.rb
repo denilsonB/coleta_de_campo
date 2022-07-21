2a5a427211193aa495674414587bb6a06f7667fa
denilsonB <denilson1569@hotmail.com>
2022-07-21 14:49:19 -0300

    add new queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,7 +6,25 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    
+    #user queries
     field :all_users, resolver: Queries::AllUsers
     field :one_user, resolver: Queries::OneUser
+
+    #visit queries
+    field :all_visits, resolver: Queries::AllVisits
+    field :one_visit, resolver: Queries::OneVisit
+
+    #formulary queries
+    field :all_formularies, resolver: Queries::AllFormularies
+    field :one_formulary, resolver: Queries::OneFormulary
+
+    #question queries
+    field :all_questions, resolver: Queries::AllQuestions
+    field :one_question, resolver: Queries::OneQuestion
+
+    #answer queries
+    field :all_answers, resolver: Queries::AllAnswers
+    field :one_answer, resolver: Queries::OneAnswer
   end
 end
