455616a27ac409fc8a8597c2498dc20a9a5ca471
denilsonB <denilson1569@hotmail.com>
2022-07-21 14:49:25 -0300

    add new mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,28 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    #Users mutations
+    field :add_user, mutation: Mutations::AddUser
+    field :update_user, mutation: Mutations::UpdateUser
+    field :destroy_user, mutation: Mutations::DestroyUser
+
+    #Visits mutations
+    field :add_visit, mutation: Mutations::AddVisit
+    field :update_visit, mutation: Mutations::UpdateVisit
+    field :destroy_visit, mutation: Mutations::DestroyVisit
+
+    #Formularies mutations
+    field :add_formulary, mutation: Mutations::AddFormulary
+    field :update_formulary, mutation: Mutations::UpdateFormulary
+    field :destroy_formulary, mutation: Mutations::DestroyFormulary
+
+    #question mutations
+    field :add_question, mutation: Mutations::AddQuestion
+    field :update_question, mutation: Mutations::UpdateQuestion
+    field :destroy_question, mutation: Mutations::DestroyQuestion
+
+    #answer mutations
+    field :add_answer, mutation: Mutations::AddAnswer
+    field :update_answer, mutation: Mutations::UpdateAnswer
+    field :destroy_answer, mutation: Mutations::DestroyAnswer
   end
 end
